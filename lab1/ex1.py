8ce8bd41df46edaadadc0218031cd61ddd5ed614
Adrian <ionicheadrian@yahoo.com>
2025-11-18 22:44:20 +0200

    .

--- lab1/ex1.py
+++
@@ -1,4 +0,0 @@
-s="BANANE BANANE BANANE BANANE PORTOCALE CASTRAVETI"
-print(f"sirul inainte de schimb este: {s}\n")
-s = s.replace("BANANE","PORTOCALE")
-print(s)
--- lab1/ex6.py
+++
@@ -1,7 +0,0 @@
-def nr_cuv(x:str):
-    cuvinte=x.split()
-    return len(cuvinte)
-
-s="Salut! Ma cheama Ady"
-
-print(nr_cuv(s))
